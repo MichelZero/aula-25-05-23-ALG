68d24df4abdcbdc6876037fd8ba831a48060e525
Michel <106674127+MichelZero@users.noreply.github.com>
2023-05-25 15:14:56 -0300

    up

---
+++ ex01.py
@@ -0,0 +1,19 @@
+#
+# 
+# 1. (OBI 2021) Escreva um programa que receba dois números 
+# inteiros como entrada e exiba a soma deles.
+
+#entrada
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+# valor2 = float(input("Digite o segundo valor: "))
+
+# processamento1
+soma = valor1 + valor2
+
+# processamento2 e saída
+# print(valor1+valor2)
+
+# saída
+# print("a soma eh", soma)
+print(f"a soma eh {soma}")
